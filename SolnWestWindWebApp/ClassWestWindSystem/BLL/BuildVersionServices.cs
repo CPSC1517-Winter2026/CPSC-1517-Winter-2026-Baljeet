87fe3ed770a2dbee3986ac6a49afd34681885665
BaljeetK79 <baljeetk@nait.ca>
2026-04-01 21:53:32 -0400

    Region service is complete: A) Class Library Project 1. Reverse Engineering 2. PoC in Linqpad 3. Service Implemented 4. Register Service
    B) WebApp
    5. Create Razor Component
    6. Razor Conponent consume relevant service
    7. Add it in Nav bar

--- SolnWestWindWebApp/ClassWestWindSystem/BLL/BuildVersionServices.cs
+++ SolnWestWindWebApp/ClassWestWindSystem/BLL/BuildVersionServices.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using ClassWestWindSystem.DAL;
+using ClassWestWindSystem.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ClassWestWindSystem.DAL;
 
 namespace ClassWestWindSystem.BLL
 {
@@ -22,5 +23,40 @@ namespace ClassWestWindSystem.BLL
         {
             _context = registered_context;
         }
+
+
+        #region Services
+        /*********************** Services *****************************/
+        // a service is a method
+        //this class will be referenced by external users (aka web app)
+        //therefore the class and the services need to be public
+
+        public BuildVersion BuildVersion_Get()
+        {
+            /*
+          * this will use the context property BuildVersions to obtain the
+          *      data from the database via the context class
+          *  the call return the dataset (DbSet) from the sql table
+          *  data returned by the query in this fashion is returned as a set
+          *      with the datatype of IEnumerable<T>, where T is the
+          *      name of the entity
+          *  the dataset create will contain 0, 1 or more records, one for
+          *      each row on your sql table
+          */
+            //get the data from the database as a collection of records
+            IEnumerable<BuildVersion> info = _context.BuildVersions;
+
+            /*
+          * data to returned is one row from the data placed within info
+          * Linq has a method that limits the number of rows from a
+          *      data collection: .FirstOrDefault()
+          * this method will return the first record in the dataset collection
+          * if lthe collection is empty it will return the default of the datatype
+          *  (in this case, it is an instance of a class, thus the default is null)
+          */
+
+            return info.FirstOrDefault();
+        }
     }
+    #endregion
 }
